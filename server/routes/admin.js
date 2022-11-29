66fbd4bee27083b211338e61587ac6edbaaa40d4
Mykola Yanchyk <mikola.yanchik@gmail.com>
2022-11-29 14:59:40 +0200

    server: add delete, update and get router for admin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -52,4 +52,44 @@ router.post("/api/admin/login", async (req, res) => {
   }
 });
 
+router.get("/api/admin/:id", async (req, res) => {
+  try {
+    const candidate = await Admin.findById(req.params.id);
+    res.status(200).json(candidate);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/api/admin/delete/:id", async (req, res) => {
+  try {
+    await Admin.findByIdAndDelete(req.params.id);
+    res.status(200).json("Deleted success");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/api/admin/update/:id", async (req, res) => {
+  try {
+    let candidate = await Admin.findByIdAndUpdate(req.params.id);
+    if (req.body.oldPass) {
+      const isMatch = await bcrypt.compare(req.body.oldPass, candidate.pass);
+      if (isMatch) {
+        candidate.pass = await bcrypt.hash(req.body.newPass, 12);
+        delete req.body.newPass;
+        delete req.body.oldPass;
+        delete req.body.pass;
+      } else return res.status(400).json("Old password is not correct");
+    }
+
+    candidate = _.extend(candidate, req.body);
+
+    await candidate.save();
+    res.status(200).json(candidate);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 module.exports = router;
